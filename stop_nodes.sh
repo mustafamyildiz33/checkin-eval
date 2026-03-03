3497c1e366787f77d2bd6bea3f1d78bfedd2089c
elswor68 <elswor68@students.rowan.edu>
2026-03-03 17:36:09 -0500

    Add files via upload

---
+++ stop_nodes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script kills all the swarm notes running under the current
+# terminal. This script should print nothing. If it prints a process
+# running a node, it means that something went wrong with the killing.
+
+
+# Kill all running nodes.
+pkill -f node.py
+
+# pkill -f "node.py 9003" 
+
+# List the processes currently running nodes
+ps -ef | grep node.py | grep -v grep
